ab99bb95335e0894811bde7a5b689e117cad2991
Agnes Font <agnesft5@gmail.com>
2019-10-18 11:38:37 +0200

    Añadidas funciones

--- index.js
+++ index.js
@@ -12,38 +12,79 @@ function httpGet(theUrl, callback) {
 }
 
 let objProduct;
-//Funció per imprimir
-function guardarJSON(text){
-   objProduct = JSON.parse(text) //objJSON
-   function buscarCH(objProduct){
 
-   }
+//Funció per guardar les dades
+function guardarDatos(text){
+   objProduct = JSON.parse(text) //objJSON
+   let product = buscarProduct(objProduct);
+   let nutrients = buscarNutrients(product);
+   //dades
+   imprimirValor(nutrients,"carbohydrates_100g");
+   imprimirValor(nutrients,"fat_100g");
+   let link = buscarImg(product);
+   
+
+// buscar elem por id o class y usar innerHTML
 }
 
-function buscarCH(objProduct){
-    for(let clave in objProduct["product"]["nutriments"]){
-        if(objProduct["product"]["nutriments"] == "carbohydrates_100g"){
-        let productCH= objProduct["product"]["nutriments"][clave];
+function imprimirValor(nutrients,nombreMacro){
+   let valor = buscarMacroNutriente(nutrients, nombreMacro);
+   let infoMacro = document.querySelector("#"+nombreMacro);
+   infoMacro.innerHTML = `${valor}g`
+}
+
+//Funció per treure el producte
+function buscarProduct(objJSON){
+    return objJSON.product;
+    /*
+    let producto ="";
+    for(let clave in objJSON){
+        if(clave == "product"){
+            producto = clave;
         }
     }
-    console.log(productCH);
-    return productCH;
-};
+    return producto;
+    */
+}
+
+//Funció per treure els nutrients
+function buscarNutrients(producto){
+    let nutriente = "";
+
+    for (let clave in producto){
+        if(clave == "nutriments"){
+            nutriente = producto[clave];
+        }
+    }
+
+    return nutriente;
+}
+
+//Funció per treure CH
+function buscarMacroNutriente(nutriente,attr){
+
+//return nutriente[attr];
+
+    let datoFinal =""
+    for (let clave in nutriente){
+        if (clave == attr){
+            datoFinal = nutriente[clave];
+        }
+    }
+    return datoFinal;
+}
+
+
+//PATH FOTO objProduct.product["selected_images"]["front"]["display"]["es"]
+//Funció per treure el objecte de les fotos
+function buscarImg(producto){
+    let link = "";
+    link = producto["selected_images"]["front"]["display"]["es"];
+    console.log(link);
+    return link;
+}
 
 
 //Impresió producte per codi de barres
 let productCode = "8422904015553"; //Codi de barres del producte
-httpGet("https://cors-anywhere.herokuapp.com/https://world.openfoodfacts.org/api/v0/product/"+productCode+".json",guardarJSON);
-
-
-
-// //Com crec que s'ha de buscar la info
-// for(let clave in objProduct["product"]["nutriments"]){
-// 	if(objProduct["product"]["nutriments"] == "carbohydrates_100g"){
-// 	console.log(objProduct["product"]["nutriments"][clave])
-// 	}
-// };
-
-
-// //El que vull treure
-// console.log(objProduct.product["nutriments"]["carbohydrates_100g"]);
+httpGet("https://cors-anywhere.herokuapp.com/https://world.openfoodfacts.org/api/v0/product/"+productCode+".json",guardarDatos);
--- index2.js
+++ index2.js
@@ -8,3 +8,14 @@ saturated-fat_100g: 0.05g
 sodium_100g: 0.07g
 sugars_100g: 6.8g
 vitamin-a_100g: 0.001346 µg
+
+// //Com crec que s'ha de buscar la info
+// for(let clave in objProduct["product"]["nutriments"]){
+// 	if(objProduct["product"]["nutriments"] == "carbohydrates_100g"){
+// 	console.log(objProduct["product"]["nutriments"][clave])
+// 	}
+// };
+
+
+// //El que vull treure
+// console.log(objProduct.product["nutriments"]["carbohydrates_100g"]);
