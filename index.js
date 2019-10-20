258c82cd6846c5d83025b80334b383c37754ad4a
Agnes Font <agnesft5@gmail.com>
2019-10-20 18:42:30 +0200

    Añadida tercera página y función para multiplicar

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ let objProduct;
 //Botons
 let enviarButton = document.querySelector(".banner__sendbutton");
 let inputCodigo = document.getElementById("codigoProducto");
-let volverButton = document.querySelector(".banner2__volverbutton");
+let cantidadButton = document.querySelector(".banner2__cantidadbutton");
+let volverButton = document.querySelector(".banner3__volverbutton");
 let divContenidor = document.querySelector(".initial__banner");
 let banner1 = document.querySelector(".body__banner");
 let banner2 = document.querySelector(".body__banner2");
+let banner3 = document.querySelector(".body__banner3");
+
 
 
 
@@ -29,23 +32,65 @@ function guardarDatos(text){
    objProduct = JSON.parse(text) //objJSON
    let product = buscarProduct(objProduct);
    let nutrients = buscarNutrients(product);
-   //dades
-   imprimirValor(nutrients,"carbohydrates_100g");
-   imprimirValor(nutrients,"fat_100g");
+   //Imprimir valor nutrients
+   let valorCarbohidratos = imprimirValor(nutrients,"carbohydrates_100g");
+   let valorGrasas = imprimirValor(nutrients,"fat_100g");
+   let valorEnergia = imprimirValor(nutrients,"energy_100g");
+   let valorAzucar = imprimirValor(nutrients,"sugars_100g");
+   let valorSaturadas = imprimirValor(nutrients,"saturated-fat_100g");
+   let valorSal = imprimirValor(nutrients,"salt_100g");
+   let valorProteinas = imprimirValor(nutrients,"proteins_100g");
+   let valorFibra = imprimirValor(nutrients,"fiber_100g");
+   //Imprimir foto
    let link = buscarImg(product);
-   let fotoProduct = document.querySelector(".card__image");
-   fotoProduct.src = link;
+   let fotoProductb2 = document.querySelector(".cardb2__image");
+   fotoProductb2.src = link;
+   let fotoProductb3 = document.querySelector(".cardb3__image");
+   fotoProductb3.src = link;
+   //Imprimir nom del producte
    let productName = buscarProductName(product);
-   let fotoProductFooter = document.querySelector(".card__footer");
-   fotoProductFooter.innerHTML = productName;
+   let fotoProductFooterb2 = document.querySelector(".cardb2__footer");
+   fotoProductFooterb2.innerHTML = productName;
+   let fotoProductFooterb3 = document.querySelector(".cardb3__footer");
+   fotoProductFooterb3.innerHTML = productName;
+   //Imprimir quantitat
+   let quantity = buscarCantidad(product);
+   let quantityProductb2 = document.querySelector(".quantity__inputb2");
+   quantityProductb2.innerHTML = ` ${quantity}`;
+   let quantityProductb3 = document.querySelector(".quantity__inputb3");
+   quantityProductb3.innerHTML = `${quantity}`;
 
-// buscar elem por id o class y usar innerHTML
-}
+   //Multiplicar per quanitat
+   let CHPorCantidad = calclularPorCantidad (valorCarbohidratos,quantity);
+   let CHCantidad = document.querySelector("#carbohydrates_cantidad");
+   CHCantidad.innerHTML = CHPorCantidad;
+   let GrasasPorCantidad = calclularPorCantidad (valorGrasas,quantity);
+   let GrasasCantidad = document.querySelector("#fat_cantidad");
+   GrasasCantidad.innerHTML = GrasasPorCantidad;
+   let EnergiaPorCantidad = calclularPorCantidad (valorEnergia,quantity);
+   let EnergiaCantidad = document.querySelector("#energy_cantidad");
+   EnergiaCantidad.innerHTML=EnergiaPorCantidad;
+   let AzucarPorCantidad = calclularPorCantidad (valorAzucar,quantity);
+   let AzucarCantidad = document.querySelector("#sugars_cantidad");
+   AzucarCantidad.innerHTML = AzucarPorCantidad;
+   let SaturadasPorCantidad = calclularPorCantidad (valorSaturadas,quantity);
+   let SaturadasCantidad = document.querySelector("#saturated-fat_cantidad");
+   SaturadasCantidad.innerHTML = SaturadasPorCantidad;
+   let SalPorCantidad = calclularPorCantidad (valorSal,quantity);
+   let SalCantidad = document.querySelector("#salt_cantidad");
+   SalCantidad.innerHTML = SalPorCantidad;
+   let ProteinasPorCantidad = calclularPorCantidad (valorProteinas,quantity);
+   let ProteinasCantidad = document.querySelector("#proteins_cantidad");
+   ProteinasCantidad.innerHTML = ProteinasPorCantidad;
+   let FibraPorCantidad = calclularPorCantidad (valorFibra,quantity);
+   let FibraCantidad = document.querySelector("#fiber_cantidad");
+   FibraCantidad.innerHTML = FibraPorCantidad;
+};
 
 function imprimirValor(nutrients,nombreMacro){
    let valor = buscarMacroNutriente(nutrients, nombreMacro);
    let infoMacro = document.querySelector("#"+nombreMacro);
-   infoMacro.innerHTML = `${valor}g`
+   infoMacro.innerHTML = `${valor}`;
 }
 
 //Funció per treure el producte
@@ -82,6 +127,8 @@ function buscarMacroNutriente(nutriente,attr){
     for (let clave in nutriente){
         if (clave == attr){
             datoFinal = nutriente[clave];
+        }else if(nutriente == undefined){
+            datoFinal = "0";
         }
     }
     return datoFinal;
@@ -92,10 +139,11 @@ function buscarImg(producto){
     let link = "";
     if (producto["selected_images"]["front"]["display"]["fr"] != undefined){
         link = producto["selected_images"]["front"]["display"]["fr"];
-    }else{
-        link = producto["selected_images"]["front"]["display"]["es"]
+    }else if (producto["selected_images"]["front"]["display"]["es"] != undefined){
+        link = producto["selected_images"]["front"]["display"]["es"];
+    }else if (producto["selected_images"]["front"]["display"]["en"] != undefined){
+        link = producto["selected_images"]["front"]["display"]["en"];
     }
-    console.log(link);
     return link;
 }
 
@@ -105,6 +153,13 @@ function buscarProductName (producto){
     productName = producto["product_name"];
     console.log(productName);
     return productName;
+}
+
+//Funció per treure el gramatge de l'aliment
+function buscarCantidad (producto){
+    let quantity ="";
+    quantity = producto["quantity"];
+    return quantity;
 }
 
 
@@ -118,15 +173,31 @@ enviarButton.addEventListener("click",()=>{
     canviarLink();
     banner1.className="banner text-center body__banner d-none";
     banner2.className="banner bg-orange text-center body__banner2";
+    banner3.className="banner bg-orange text-center body__banner3 d-none";
+});
+
+cantidadButton.addEventListener("click",()=>{
+    banner1.className="banner text-center body__banner d-none";
+    banner2.className="banner bg-orange text-center body__banner2 d-none";
+    banner3.className="banner bg-orange text-center body__banner3"
+    ereseValue();
 });
 
 volverButton.addEventListener("click",()=>{
     banner1.className="banner text-center body__banner";
     banner2.className="banner bg-orange text-center body__banner2 d-none";
+    banner3.className="banner bg-orange text-center body__banner3 d-none"
     ereseValue();
 });
 
 function ereseValue(){
     inputCodigo.value="";
 };
+
+function calclularPorCantidad (valor,cantidad){
+    let resultadoPorCantidad = (valor/100)*cantidad;
+    return resultadoPorCantidad;
+};
+
 //Codi:8422904015553
+//737628064502
